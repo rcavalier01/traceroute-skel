b689f41a4cce8bec68f93e35ed69f8ea1af98897
AWS User <aws.admin@nc-mns11-app080.apporto.com>
2025-11-09 17:39:23 -0800

    distinguishing icmp

--- traceroute.cpp
+++ traceroute.cpp
@@ -154,11 +154,16 @@ int main (int argc, char *argv[]) {
 
       // i. Use select() to sleep for up to 5 seconds, wake up if data arrives.
     int poll = select(recSockFD+1, &mySet, NULL, NULL, &timeout);
-    if(poll < 0){
+    if(poll == 0){
+      printf("%d ***timeout", current_ttl);
+      continue;
+    }else if(poll < 0){
       perror("select");
-    break;
-    }
+      break;
+    
+    
       // ii. If data has arrived, read it with recevfrom()
+      if(FD_ISSET(recSockFD, &mySet)){
         // 1. If received data is Echo Reply from the destination
           // a. Print message
           // b. Set reply-not-received to false
@@ -166,6 +171,24 @@ int main (int argc, char *argv[]) {
         // 2. If received data is TTL Time Exceeded; TTL
           // a. print message
           // b. Set not-done-reading to false
+        struct sockaddr_in rec_addr;
+        socklen_t r_length = sizeof(rec_addr);
+        ssize_t bytes_read = recvfrom(recSockFD, recBuff, sizeof(recBuff), 0, (struct sockaddr *)&rec_addr, &r_length);
+        if(bytes_read < 0){
+          perror("recvfrom");
+          continue;
+        }
+        char respAddress[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &rec_addr.sin_addr, respAddress, INET_ADDRSTRLEN);
+        printf("%d %s\n", current_ttl, respAddress);
+        auto current = rec_addr.sin_addr.s_addr;
+        auto desired = dest_addr.sin_addr.s_addr;
+        if(current == desired){
+          printf("yay!\n");
+          break;
+        }
+      }
+        
     // e. Increment TTL by 1.
   }
  
@@ -174,7 +197,8 @@ int main (int argc, char *argv[]) {
   //inet_ntop(AF_INET, &recv_addr.sin_addr, respondent_ip, INET_ADDRSTRLEN);
 
   int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timout);
-
+  close(sendSockFD);
+  close(recSockFD);
   delete[] sendBuff;
   delete[] recBuff;
   return 0;
