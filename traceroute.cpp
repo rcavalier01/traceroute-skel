5bd1d853b28e25643a71befcb6376ede31c7f66d
AWS User <aws.admin@nc-mns11-app080.apporto.com>
2025-11-09 20:29:37 -0800

    doesnt work like at all, reapeating and hops at 2 still with incorrect address

--- traceroute.cpp
+++ traceroute.cpp
@@ -126,10 +126,11 @@ int main (int argc, char *argv[]) {
   memset(&dest_addr, 0, sizeof(dest_addr));
   dest_addr.sin_family = AF_INET;
   dest_addr.sin_addr.s_addr = inet_addr(destIP.c_str());
-
+  bool responseGot = false;
   // 6. while (CURRENT_TTL <= 31) and (reply-not-received)
+  // TTL LOOP START ##############################################
   for(int current_ttl = 2; current_ttl <=31; current_ttl++){
-    bool responseGot = false;
+    
     // a. Set the TTL in the IP header in the buffer to CURRENT_TTL
     struct iphdr *ip_header = (struct iphdr *)sendBuff;
     struct icmp *icmp_header = (struct icmp *)(sendBuff+sizeof(struct iphdr));
@@ -149,7 +150,9 @@ int main (int argc, char *argv[]) {
     // d. While (now < START_TIME + 15) and (not-done-reading)
     struct timeval timeout;
     fd_set mySet;
-    for(int i = 0; i<3 && !responseGot; i++){
+    bool doneReading = false;
+    // POLLING LOOP START #####################################
+    for(int i = 0; i<3 && !doneReading; i++){
       FD_ZERO(&mySet);
       FD_SET(recSockFD, &mySet);
       timeout.tv_sec = 5;
@@ -157,63 +160,65 @@ int main (int argc, char *argv[]) {
       // i. Use select() to sleep for up to 5 seconds, wake up if data arrives.
       int poll = select(recSockFD+1, &mySet, NULL, NULL, &timeout);
       if(poll == 0){
-        printf("%d ***timeout", current_ttl);
+        if(i == 2){
+          printf("%d *** no response from poll after 15 seconds ***\n", current_ttl );
+        }
         continue;
       }else if(poll < 0){
         perror("select");
+        doneReading = true;
         break;
       }else{
-        responseGot = true;
+        printf("doing nothing = true\n");
       }
-    }
-    if(!responseGot){
-      printf("%d *** no response from poll after 15 seconds ***\n", current_ttl );
-    }
-    
-      // ii. If data has arrived, read it with recevfrom()
-    if(FD_ISSET(recSockFD, &mySet)){
-      struct sockaddr_in rec_addr;
-      socklen_t r_length = sizeof(rec_addr);
-      ssize_t bytes_read = recvfrom(recSockFD, recBuff, sizeof(recBuff), 0, (struct sockaddr *)&rec_addr, &r_length);
-      if(bytes_read < 0){
-          perror("recvfrom");
-          continue;
-      }
-      char respAddress[INET_ADDRSTRLEN];
-      inet_ntop(AF_INET, &rec_addr.sin_addr, respAddress, INET_ADDRSTRLEN);
-        // 1. If received data is Echo Reply from the destination
-      struct iphdr *rec_ip = (struct iphdr *)recBuff;
-      struct icmp *rec_icmp = (struct icmp *)(recBuff + (rec_ip->ihl*4)) ;
-      if(rec_icmp->icmp_type == ICMP_ECHOREPLY){
+
+      if(FD_ISSET(recSockFD, &mySet)){
+        struct sockaddr_in rec_addr;
+        socklen_t r_length = sizeof(rec_addr);
+        ssize_t bytes_read = recvfrom(recSockFD, recBuff, sizeof(recBuff), 0, (struct sockaddr *)&rec_addr, &r_length);
+        if(bytes_read < 0){
+            perror("recvfrom");
+            continue;
+        }
+        char respAddress[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &rec_addr.sin_addr, respAddress, INET_ADDRSTRLEN);
+          // 1. If received data is Echo Reply from the destination
+        struct iphdr *rec_ip = (struct iphdr *)recBuff;
+        struct icmp *rec_icmp = (struct icmp *)(recBuff + (rec_ip->ihl*4)) ;
+        if(rec_icmp->icmp_type == ICMP_ECHOREPLY){
           // a. Print message
           // b. Set reply-not-received to false
           // c. Set not-done-reading to false
-        printf("%d %s completed\n", current_ttl, respAddress);
-        break;
-      }else if(rec_icmp->icmp_type == ICMP_TIME_EXCEEDED){
+          printf("%d %s completed\n", current_ttl, respAddress);
+          responseGot = true;
+        }else if(rec_icmp->icmp_type == ICMP_TIME_EXCEEDED){
           // 2. If received data is TTL Time Exceeded; TTL
           // a. print message
           // b. Set not-done-reading to false
           printf("%d %s TTL exceeded\n", current_ttl, respAddress);
-          break;
-      }else{
-        printf("TTL: %2d, Response Address: %s, Weird ICMP type: %d \n", current_ttl, respAddress, rec_icmp->icmp_type);
-        break;
+          responseGot = true;
+        }else{
+          printf("TTL: %2d, Response Address: %s, Weird ICMP type: %d \n", current_ttl, respAddress, rec_icmp->icmp_type);
+          responseGot = true;
+        }
+        
+        //auto current = rec_addr.sin_addr.s_addr;
+        //auto desired = dest_addr.sin_addr.s_addr;
+        //if(current == desired){
+        //  printf("yay!\n");
+        //  break;
+        //}
       }
-        
-        
-        
+    }//POLLLING LOOP END ####################################
+    //if(!responseGot){
+    //  printf("%d *** no response from poll after 15 seconds ***\n", current_ttl );
+    //}
     
-      //auto current = rec_addr.sin_addr.s_addr;
-      //auto desired = dest_addr.sin_addr.s_addr;
-      //if(current == desired){
-      //  printf("yay!\n");
-      //  break;
-      //}
-    }
+      // ii. If data has arrived, read it with recevfrom()
+    
         
     // e. Increment TTL by 1.
-  }
+  }//TTL LOOP END ###############
  
  
   //Convert an address structure to a dotted decimal string.
